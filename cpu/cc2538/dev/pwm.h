c788ad14ed05db88e2f7de7f4caa64b0fe20aad3
Brad Campbell <bradjc5@gmail.com>
2015-04-26 17:22:15 -0400

    updated pwm

--- cpu/cc2538/dev/pwm.h
+++ cpu/cc2538/dev/pwm.h
@@ -3,9 +3,10 @@
 
 #include <stdint.h>
 
-int pwm_init(uint8_t gptimer, uint8_t gpsubtimer, uint32_t freq, uint8_t port, uint8_t pin);
+int pwm_init(uint8_t gptimer, uint8_t gpsubtimer, uint32_t freq, uint32_t dc, uint8_t port, uint8_t pin);
 void pwm_start(uint8_t gptimer, uint8_t gpsubtimer);
 void pwm_stop(uint8_t gptimer, uint8_t gpsubtimer);
+int pwm_set_frequency(uint8_t gptimer, uint8_t gpsubtimer, uint32_t freq);
 int pwm_set_dutycycle(uint8_t gptimer, uint8_t gpsubtimer, uint32_t dc);
 
 #endif
